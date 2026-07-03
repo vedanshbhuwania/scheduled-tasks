5032635ffc1092328fa63d3dc8c58c088e5ed0c9
vedanshbhuwania <vedanshbhuwania@gmail.com>
2026-07-03 09:39:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,35 +4,32 @@
 # 3. Update the SMTP ADDRESS to match your email provider.
 # 4. Update birthdays.csv to contain today's month and day.
 # See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
 import os
-
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
-
-today = datetime.now()
-today_tuple = (today.month, today.day)
-
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
-
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+import requests
+import smtplib
+api_key="323554271b6141e671455bfa232d9a4f"
+api_key_secret=os.environ.get("API_KEY")
+parameters={
+    "lat":22.565573,
+    "lon":88.370215,
+    "appid":api_key_secret,
+    "cnt":4,
+}
+response=requests.get(url="https://api.openweathermap.org/data/2.5/forecast",params=parameters)
+print(response.status_code)
+response.raise_for_status()
+weather_data=response.json()
+weather_id=[]
+for i in range(4):
+    weather_id.append(weather_data["list"][i]["weather"][0]["id"])
+print(weather_id)
+for id in weather_id:
+    if id<700:
+        connection=smtplib.SMTP("smtp.gmail.com",port=587)
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+        my_email="rashibhuwania20@gmail.com"
+        password="kzzoaqehjyidxzrc"
+        connection.login(user=my_email,password=password)
+        connection.sendmail(from_addr="unknown_man@gmail.com",to_addrs=my_email,msg="Its going to Rain . Bring an umbrella")
+        connection.close()
+        break
